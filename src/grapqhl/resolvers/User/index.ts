19c232b7966631f38d67551b310d515851ab78ea
Hafidh Pradipta A <hamstergeek38@gmail.com>
2021-12-03 17:04:55 +0700

    implement user resolver

---
+++ src/grapqhl/resolvers/User/index.ts
@@ -0,0 +1,100 @@
+import { IResolvers } from "apollo-server-express";
+import { Request } from "express";
+import { Database, User } from "../../../lib/types";
+import { authorize } from "../../../lib/utils";
+import {
+  UserArgs,
+  UserBookingsArgs,
+  UserBookingsData,
+  UserListingsArgs,
+  UserListingsData,
+} from "./types";
+
+export const userResolver: IResolvers = {
+  Query: {
+    user: async (
+      _root: undefined,
+      { id }: UserArgs,
+      { db, req }: { db: Database; req: Request }
+    ): Promise<User | null> => {
+      try {
+        const user = await db.users.findOne({ _id: id });
+        console.log(user);
+        if (!user) {
+          throw new Error("We can't find user");
+        }
+
+        const viewer = await authorize(db, req);
+
+        if (viewer && viewer._id === user._id) {
+          user.authorized = true;
+        }
+
+        return user;
+      } catch (error) {
+        throw new Error(`Failed to query user: ${error}`);
+      }
+    },
+  },
+  User: {
+    id: (user: User) => user._id,
+    hasWallet: (user: User) => Boolean(user.walletId),
+    income: (user: User) => (user.authorized ? user.income : null),
+    bookings: async (
+      user: User,
+      { limit, page }: UserBookingsArgs,
+      { db }: { db: Database }
+    ): Promise<UserBookingsData | null> => {
+      try {
+        if (!user.authorized) {
+          return null;
+        }
+
+        const data: UserBookingsData = {
+          total: 0,
+          result: [],
+        };
+
+        const bookings = await db.bookings
+          .find({
+            _id: { $in: user.bookings },
+          })
+          .skip(page > 0 ? (page - 1) * 10 : 0)
+          .limit(limit);
+
+        data.total = await bookings.count();
+        data.result = await bookings.toArray();
+
+        return data;
+      } catch (error) {
+        throw new Error(`We can't query bookings: ${error}`);
+      }
+    },
+    listings: async (
+      user: User,
+      { limit, page }: UserListingsArgs,
+      { db }: { db: Database }
+    ): Promise<UserListingsData | null> => {
+      try {
+        const data: UserListingsData = {
+          total: 0,
+          result: [],
+        };
+
+        const listings = await db.listings
+          .find({
+            _id: { $in: user.listings },
+          })
+          .skip(page > 0 ? (page - 1) * 10 : 0)
+          .limit(limit);
+
+        data.total = await listings.count();
+        data.result = await listings.toArray();
+
+        return data;
+      } catch (error) {
+        throw new Error(`We can't query listingss: ${error}`);
+      }
+    },
+  },
+};
--- src/grapqhl/typeDefs.ts
+++ src/grapqhl/typeDefs.ts
@@ -5,6 +5,58 @@ export const typeDefs = gql`
     code: String
   }
 
+  enum ListingType {
+    APARTMENT
+    HOUSE
+  }
+
+  type Booking {
+    id: ID!
+    listing: Listing!
+    tenant: User!
+    checkIn: String!
+    checkOut: String!
+  }
+
+  type Bookings {
+    total: Int!
+    result: [Booking!]!
+  }
+
+  type Listing {
+    id: ID!
+    title: String!
+    description: String!
+    image: String!
+    host: User!
+    type: ListingType!
+    address: String!
+    country: String!
+    admin: String!
+    city: String!
+    bookings(limit: Int!, page: Int!): Bookings
+    bookingsIndex: String!
+    price: Int!
+    numOfGuests: Int!
+  }
+
+  type Listings {
+    total: Int!
+    result: [Listing!]!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    token: String!
+    avatar: String!
+    contact: String!
+    hasWallet: Boolean!
+    income: Int
+    bookings(limit: Int!, page: Int!): Bookings
+    listings(limit: Int!, page: Int!): Listings!
+  }
+
   type Viewer {
     id: ID
     avatar: String
@@ -15,6 +67,7 @@ export const typeDefs = gql`
 
   type Query {
     authUrl: String!
+    user(id: ID!): User!
   }
 
   type Mutation {
