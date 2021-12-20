2de31fbcc886eb2c8fb05ad7522838b74b653af0
Hafidh Pradipta A <hamstergeek38@gmail.com>
2021-12-21 00:14:59 +0700

    fix

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -7,7 +7,7 @@ export const authorize = async (
 ): Promise<User | null> => {
   const token = req.get("X-CSRF-TOKEN");
   const viewer = await db.users.findOne({
-    _id: req.signedCookies.viewer,
+    _id: req.session.viewer,
     token,
   });
 
